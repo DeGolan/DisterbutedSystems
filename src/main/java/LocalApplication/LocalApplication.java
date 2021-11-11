21bead774bcc468bf76d0944bae5ba9519a944f1
Omer <omergo66@gmail.com>
2021-11-11 10:06:37 -0500

    startManager & uploadPDFListToS3 done

--- src/main/java/LocalApplication/LocalApplication.java
+++ src/main/java/LocalApplication/LocalApplication.java
@@ -2,6 +2,7 @@ package LocalApplication;
 
 import java.io.IOException;
 import java.nio.file.Paths;
+import java.util.List;
 
 import software.amazon.awssdk.core.sync.RequestBody;
 import software.amazon.awssdk.regions.Region;
@@ -22,58 +23,42 @@ public class LocalApplication {
 
         Region region = Region.US_EAST_1;
 
-//        //uploading the pdf_src to the s3
-//        S3Client s3 = S3Client.builder().region(region).build();
-//        String bucket = "dsps12bucket";
-//        String key = "pdf_src";
-//        System.out.println("Uploading pdf source file to S3...");
-//        s3.putObject(PutObjectRequest.builder().bucket(bucket).key(key).build(),
-//                RequestBody.fromFile(Paths.get("/home/vagrant/DistributedSystems/src/main/resources/input-sample-1.txt")));
-//        System.out.println("Upload complete");
-//        System.out.printf("%n");
-//        System.out.println("Closing the connection to {S3}");
-//        s3.close();
-//        System.out.println("Connection closed");
+        //uploadPDFListToS3(region);
 
-
-        Ec2Client ec2 = Ec2Client.builder()
-                .region(region)
-                .build();
-//        String instanceId="i-04645567067640ab5";
-//        StartInstancesRequest request = StartInstancesRequest.builder()
-//                .instanceIds(instanceId)
+//        Ec2Client ec2 = Ec2Client.builder()
+//                .region(region)
 //                .build();
-//            ec2.startInstances(request);
-//            System.out.printf("Successfully stopped instance %s", instanceId);
-
-        if(!findManager(ec2)){
-
-        };
-            ec2.close();
+//
+//        startManager(ec2);
+//        ec2.close();
     }
-    public static boolean findManager2(Ec2Client ec2) {
-        Filter filter = Filter.builder().name("instance-state-name").values()
-    }
+
     //check if the instance tag is "Manager"
-    public static boolean findManager(Ec2Client ec2) {
+    public static void startManager(Ec2Client ec2) {
         try {
             String nextToken = null;
             do {
-                Filter filter = Filter.builder()
-                        .name()
-                        .values("running")
-                        .build();
-
-                DescribeInstancesRequest request = DescribeInstancesRequest.builder()
-                        .filters(filter)
-                        .build();
+                DescribeInstancesRequest request = DescribeInstancesRequest.builder().build();
                 DescribeInstancesResponse response = ec2.describeInstances(request);
+
                 for (Reservation reservation : response.reservations()) {
                     for (Instance instance : reservation.instances()) {
-                       if(instance.tags().get(0).key().equals("Manager")){
-                           return true;
+                       List <Tag> tags=instance.tags();
+                       if(tags.size()>0){
+                           if(tags.get(0).key().equals("Manager")){
+                              if(!instance.state().name().toString().equals("running")){
+                                  String instanceId=instance.instanceId();
+                                  StartInstancesRequest request2 = StartInstancesRequest.builder()
+                                        .instanceIds(instanceId)
+                                        .build();
+                                  ec2.startInstances(request2);
+                                  System.out.printf("Successfully started Manager");
+                              }else {
+                                  System.out.printf("Manager is already running");
+                              }
+                              return;//only have one manager, can stop iterate
+                           }
                        }
-
                     }
                 }
                 nextToken = response.nextToken();
@@ -83,6 +68,21 @@ public class LocalApplication {
             System.err.println(e.awsErrorDetails().errorMessage());
             System.exit(1);
         }
-        return  false;
+
+    }
+
+    //uploading the pdf_src to the s3
+    public static void uploadPDFListToS3(Region region){
+        S3Client s3 = S3Client.builder().region(region).build();
+        String bucket = "dsps12bucket";
+        String key = "pdf_src";
+        System.out.println("Uploading pdf source file to S3...");
+        s3.putObject(PutObjectRequest.builder().bucket(bucket).key(key).build(),
+                RequestBody.fromFile(Paths.get("/home/vagrant/DistributedSystems/src/main/resources/input-sample-1.txt")));
+        System.out.println("Upload complete");
+        System.out.printf("%n");
+        System.out.println("Closing the connection to {S3}");
+        s3.close();
+        System.out.println("Connection closed");
     }
 }
